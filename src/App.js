6839ce80be4eba31c01a84d0c1c3bc19555fbf03
Amantur Kadyrkulov <emptyline03@gmail.com>
2019-11-11 18:06:21 +0600

    Сделал логику в app.js и изменил стили в app.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Num from './Components/Random36/random36'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends Component{
+    state ={
+        numbers : [0,0,0,0,0],
+    };
+    ranNum = () => Math.floor(Math.random() * (37 - 5) + 5);
+
+    newNumbers = () => {
+        let numArr = [];
+        for(let i = 1; i < 6;){
+            let number = this.ranNum();
+            if(numArr.includes(number)){
+                continue;
+            }
+            numArr.push(number);
+            ++i;
+        }
+        numArr.sort(function (x, y) {
+            return x - y ;
+        });
+        let newArr = {...this.state};
+        newArr.numbers = numArr;
+        let numbers = newArr.numbers;
+        this.setState({numbers});
+    };
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <button onClick={this.newNumbers}>New Numbers</button>
+                    <div className="row">
+                        <Num number={this.state.numbers[0]}/>
+                        <Num number={this.state.numbers[1]}/>
+                        <Num number={this.state.numbers[2]}/>
+                        <Num number={this.state.numbers[3]}/>
+                        <Num number={this.state.numbers[4]}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
